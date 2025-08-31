cf4a625fb16a470d2d9051d87c73a29652c37ce8
Camden Bock <camden.bock@maine.edu>
2025-08-31 18:05:20 -0400

    working on l3build process

--- Makefile
+++ Makefile
@@ -1,39 +1,26 @@
-# Define the source files and the main executable
-MAIN_DTX = maine-thesis.dtx
-INS_FILE = maine-thesis.ins
-CLS_FILE = maine-thesis.cls
-MWE_DIR = mwe/
-TEMPLATE_DIR = template/
-EXAMPLE_FILE = $(MWE_DIR)maine-thesis-example.tex
-TEMP_DTX = $(CLS_FILE).tmp.dtx
+.PHONY: all doc clean unpack ctan
 
-# The main target to build everything
-all: $(CLS_FILE)  $(EXAMPLE_FILE) copy-cls
+# Main target: Unpack sources, build documentation, and prepare for release.
+all: unpack doc
 
-# The following lines MUST be indented with a single TAB character
-$(CLS_FILE): $(INS_FILE) $(MAIN_DTX)
-	@echo "--- Running docstrip to create $(CLS_FILE) ---"
-	lualatex $(INS_FILE)
-	lualatex $(MAIN_DTX)
+# Unpacks the DTX files into the working directory.
+unpack:
+	@echo "--- Unpacking sources ---"
+	@l3build unpack
 
-# Rule to copy the generated .cls file to the mwe directory
-copy-cls: $(CLS_FILE)
-	@echo "--- Copying $(CLS_FILE) to $(MWE_DIR) ---"
-	cp $(CLS_FILE) $(MWE_DIR)
-	cp $(CLS_FILE) $(TEMPLATE_DIR)
+# Builds the main documentation PDF.
+doc:
+	@echo "--- Building documentation ---"
+	@l3build doc
 
-# Rule to compile the user example and run biber
-$(EXAMPLE_FILE): $(CLS_FILE)
-	@echo "--- Compiling example document with lualatex ---"
-	lualatex $(EXAMPLE_FILE)
-
-# Clean up temporary files
+# Cleans up temporary and generated files.
 clean:
-	rm -f *.log *.aux *.out *.pdf *.fls *.cls.tmp.dtx *.cls
-	@echo "--- Cleaned up all generated files. ---"
+	@echo "--- Cleaning up files ---"
+	@l3build clean
+	@rm -f *.pdf *.cls *.sty *.log *.aux *.bbl *.bcf *.blg *.fdb_latexmk *.fls *.synctex.gz *.out *.lot *.lol *.lof *.toc
+	@rm -f myclass*.zip
 
-# The distclean target removes all generated files, including the .cls and PDF
-distclean: clean
-	rm -f $(CLS_FILE) $(MWE_DIR)$(CLS_FILE) $(MWE_DIR)*.pdf
-	rm -f $(MWE_DIR)*.aux $(MWE_DIR)*.bbl $(MWE_DIR)*.bcf $(MWE_DIR)*.blg $(MWE_DIR)*.log $(MWE_DIR)*.run.xml
-	@echo "--- Performed a thorough clean of all generated files. ---"
+# Prepares a CTAN-ready TDS archive.
+ctan:
+	@echo "--- Preparing CTAN package ---"
+	@l3build ctan
---
+++ build.lua
@@ -0,0 +1,73 @@
+-- Identify the bundle and main module
+bundle = "maine-thesis"
+module = "maine-thesis"
+
+-- Specify the directory where the main source files (.dtx, .ins) are located
+sourcefiledir = "sources"
+
+-- Identify all source files to be processed
+sourcefiles = {
+    "maine-thesis.dtx",
+    "pagestyle.dtx",
+    "options.dtx",
+    "requirements.dtx",
+    "variables.dtx",
+    "depreciated.dtx",
+    "format/captions.dtx",
+    "format/general-format.dtx",
+    "format/headings.dtx",
+    "format/pagestyle.dtx",
+    "format/titlepage.dtx",
+    "format/toc.dtx",
+    "environments/abstract.dtx",
+    "environments/acknowledgements.dtx",
+    "environments/appendix.dtx",
+    "environments/biography.dtx",
+    "environments/copyright.dtx",
+    "environments/dedication.dtx",
+    "environments/main-seperator.dtx",
+    "environments/preface.dtx",
+    "environments/references.dtx"
+}
+
+-- Identify files to be installed. These are the *generated* files, and
+-- l3build will expect them in the main directory after running `l3build unpack`.
+installfiles = {
+    "maine-thesis.cls",
+    "maine-thesis-pagestyle.sty",
+    "maine-thesis-options.sty",
+    "maine-thesis-requirements.sty",
+    "maine-thesis-variables.sty",
+    "maine-thesis-depreciated.sty",
+    "maine-thesis-caption.sty",
+    "maine-thesis-general-format.sty",
+    "maine-thesis-headings.sty",
+    "maine-thesis-pagestyle.sty",
+    "maine-thesis-titlepage.sty",
+    "maine-thesis-contents.sty",
+    "maine-thesis-abstract.sty",
+    "maine-thesis-acknowledgements.sty",
+    "maine-thesis-appendix.sty",
+    "maine-thesis-biography.sty",
+    "maine-thesis-copyright.sty",
+    "maine-thesis-dedication.sty",
+    "maine-thesis-main-seperator.sty",
+    "maine-thesis-preface.sty",
+    "maine-thesis-references.sty"
+}
+
+-- Set the name for the main documentation.
+-- We'll assume the main documentation is compiled from myclass.dtx
+typesetfile = {"maine-thesis.dtx"}
+
+-- This is the new part for handling examples and templates!
+-- `demofiles` lists files that should be packaged as examples.
+-- The path is relative to the directory where the command is run.
+-- You can use wildcards here.
+demofiles = {
+    install = "example", -- The directory in the TDS archive
+    files = {
+        "mwe/maine-thesis-example.tex",
+        "template/main.tex"
+    }
+}
