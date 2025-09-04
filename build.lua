39bf4dfdd07e19655d94691ac3a1934f71ee3fc9
Camden Bock <camden.bock@maine.edu>
2025-09-04 10:19:00 -0400

    update build pipeline

--- build.lua
+++ build.lua
@@ -1,223 +1,53 @@
---[[
-  ** Build config for maine-thesis using l3build **
---]]
+# Use a modern TeX Live image that also includes the 'make' utility.
+variables:
+  LATEX_IMAGE: listx/texlive:2024
 
--- Identify the bundle and main module
-module = "maine-thesis"
-pkgversion = "2.0"
-pkgdate    = "2025-09-01"
+# Define the stages for the pipeline: first build, then release.
+stages:
+  - build
+  - release
 
--- Specify the directory where the main source files (.dtx, .ins) are located
-maindir        = "."
-sourcefiledir = "./sources"
-textfiledir   = "./sources"
-textfiles     = {textfiledir.."/CTANREADME.md"}
-sourcefiles   = {"maine-thesis.dtx"}
-installfiles  = {"maine-thesis.pdf", "maine-thesis.cls", "example.tex", "example.pdf"}
-tdslocations  = {
-  "source/latex/maine-thesis/maine-thesis.dtx",
-  "doc/latex/maine-thesis/example/example.tex",
-  "doc/latex/maine-thesis/example/example.pdf",
-  "doc/latex/maine-thesis/maine-thesis.pdf",
-  "tex/latex/maine-thesis/maine-thesis.sty"
-}
+# This job runs 'make all' and saves the resulting files as artifacts.
+build_assets:
+  stage: build
+  image: $LATEX_IMAGE
+  script:
+    # We assume your Makefile's "all" target creates all the necessary files.
+    - make all
+  artifacts:
+    # Define the specific files you want to keep and pass to the release job.
+    paths:
+      - build/unpacked/maine-thesis.cls
+      - maine-thesis.tds.zip
+      - "maine-thesis-*.zip"
+      - template.zip
+    # Expire artifacts after 1 week to save space.
+    expire_in: 1 week
+  # This job will run on all commits except tags.
+  except:
+    - tags
 
-unpackfiles = {"maine-thesis.dtx"}
-unpackopts  = "--interaction=batchmode"
-unpackexe   = "luatex"
-
--- Set the name for the main documentation.
--- We'll assume the main documentation is compiled from myclass.dtx
-typesetfiles  = {"maine-thesis.dtx", "example.tex"}
-typesetexe    = "lualatex"
-typesetopts   = "--interaction=batchmode"
-typesetruns   = 3
-makeindexopts = "-q"
-
--- Build example.tex using lualatex (one run)
--- This function is moved to be defined before it is used.
-local function type_example()
-  local file = jobname(unpackdir.."/example.tex")
-  errorlevel = run(unpackdir, "lualatex --interaction=batchmode "..file..".tex > "..os_null)
-  if errorlevel ~= 0 then
-    error("** Error!!: lualatex --interaction=batchmode "..file..".tex")
-    return errorlevel
-  else
-    print("** Running: lualatex --interaction=batchmode "..file..".tex")
-  end
-  return 0
-end
-
-specialtypesetting = { }
-specialtypesetting["example.tex"]= {func = type_example}
-
--- Update package date and version
-tagfiles = {"maine-thesis.dtx", "CTANREADME.md"}
-local mydate = os.date("!%Y-%m-%d")
-
-function update_tag(file, content, tagname, tagdate)
-  if not tagname and tagdate == mydate then
-    tagname = pkgversion
-    tagdate = pkgdate
-    print("** "..file.." have been tagged with the version and date of build.lua")
-  else
-    local v_maj, v_min = string.match(tagname, "^v?(%d+)(%S*)$")
-    if v_maj == "" or not v_min then
-      print("Error!!: Invalid tag '"..tagname.."', none of the files have been tagged")
-      os.exit(0)
-    else
-      tagname = string.format("%i%s", v_maj, v_min)
-      tagdate = mydate
-    end
-    print("** "..file.." have been tagged with the version "..tagname.." and date "..mydate)
-  end
-
-  if string.match(file, "maine-thesis.dtx") then
-    local tagdate = string.gsub(tagdate, "-", "/")
-    content = string.gsub(content,
-                          "%[%d%d%d%d%/%d%d%/%d%d%s+v%S+",
-                          "["..tagdate.." v"..tagname)
-  end
-
-  if string.match(file, "CTANREADME.md") then
-    local tagdate = string.gsub(tagdate, "/", "-")
-    content = string.gsub(content,
-                          "Version: (%d+)(%S+)",
-                          "Version: "..tagname)
-    content = string.gsub(content,
-                          "Date: %d%d%d%d%-%d%d%-%d%d",
-                          "Date: "..tagdate)
-  end
-  return content
-end
-
--- Configuration for ctan
-ctanreadme = "CTANREADME.md"
-ctanpkg    = "maine-thesis"
-ctanzip    = ctanpkg.."-"..pkgversion
-packtdszip = true
-
--- Load personal data for ctan upload
-local ok, mydata = pcall(require, "mypersonaldata.lua")
-if not ok then
-  mydata = { email="XXX", uploader="YYY", }
-end
-
-uploadconfig = {
-  author      = "Your name",
-  uploader    = mydata.uploader,
-  email       = mydata.email,
-  pkg         = ctanpkg,
-  version     = pkgversion,
-  license     = "lppl1.3c",
-  summary     = "Document Class for University of Maine Graduate Thesis",
-  description = [[A Document Class for Thesis Formatting at the University of Maine]],
-  topic       = { "thesis" },
-  ctanPath    = "/macros/latex/contrib/" .. ctanpkg,
-  repository  = "https://github.com/yourrepo/maine-thesis-jw",
-  bugtracker  = "https://github.com/yourrepo/maine-thesis-jw/issues",
-  support     = "https://github.com/yourrepo/maine-thesis-jw/issues",
-  announcement_file="ctan.ann",
-  note_file   = "ctan.note",
-  update      = true,
-}
-
--- Clean files
-cleanfiles = {
-  ctanzip..".curlopt",
-  ctanzip..".zip",
-  "example.log",
-  "example.pdf",
-  "maine-thesis.pdf",
-}
-
--- Line length in 80 characters
-local function os_message(text)
-  local mymax = 77 - string.len(text) - string.len("done")
-  local msg = text.." "..string.rep(".", mymax).." done"
-  return print(msg)
-end
-
--- Create check_marked_tags() function
-local function check_marked_tags()
-  local f = assert(io.open("sources/maine-thesis.dtx", "r"))
-  marked_tags = f:read("*all")
-  f:close()
-
-  local m_pkgd, m_pkgv = string.match(marked_tags, "%[(%d%d%d%d%/%d%d%/%d%d)%s+v(%S+)")
-  local pkgdate = string.gsub(pkgdate, "-", "/")
-  if pkgversion == m_pkgv and pkgdate == m_pkgd then
-    os_message("** Checking version and date in maine-thesis.dtx: OK")
-  else
-    print("** Warning: maine-thesis.dtx is marked with version "..m_pkgv.." and date "..m_pkgd)
-    print("** Warning: build.lua is marked with version "..pkgversion.." and date "..pkgdate)
-    print("** Check version and date in build.lua then run l3build tag")
-  end
-end
-
--- Config tag_hook
-function tag_hook(tagname)
-  check_marked_tags()
-end
-
--- Add "tagged" target to l3build CLI
-if options["target"] == "tagged" then
-  check_marked_tags()
-  os.exit()
-end
-
--- Create make_temp_dir() function
-local function make_temp_dir()
-  -- Fix basename(path) in windows (https://chat.stackexchange.com/transcript/message/55064157#55064157)
-  local function basename(path)
-    return path:match("^.*[\\/]([^/\\]*)$")
-  end
-  local tmpname = os.tmpname()
-  tempdir = basename(tmpname)
-  errorlevel = mkdir(tempdir)
-  if errorlevel ~= 0 then
-    error("** Error!!: The ./"..tempdir.." directory could not be created")
-    return errorlevel
-  else
-    os_message("** Creating the temporary directory ./"..tempdir..": OK")
-  end
-end
-
--- Add "testpkg" target to l3build CLI
-if options["target"] == "testpkg" then
-  make_temp_dir()
-  errorlevel = cp("*.*", sourcefiledir, tempdir)
-  if errorlevel ~= 0 then
-    error("** Error!!: Can't copy files from "..sourcefiledir.." to /"..tempdir)
-    return errorlevel
-  else
-    os_message("** Copying files from "..sourcefiledir.." to ./"..tempdir..": OK")
-  end
-  -- Unpack files
-  local file = jobname(tempdir.."/maine-thesis.dtx")
-  errorlevel = run(tempdir, "pdftex -interaction=batchmode "..file..".dtx > "..os_null)
-  if errorlevel ~= 0 then
-    error("** Error!!: pdftex -interaction=batchmode "..file..".dtx")
-    return errorlevel
-  else
-    os_message("** Running: pdftex -interaction=batchmode "..file..".dtx")
-  end
-  -- pdflatex
-  local file = jobname(tempdir.."/example.tex")
-  errorlevel = run(tempdir, "pdflatex -interaction=nonstopmode "..file.." > "..os_null)
-  if errorlevel ~= 0 then
-    error("** Error!!: pdflatex -interaction=nonstopmode "..file..".tex")
-    return errorlevel
-  else
-    os_message("** Running: pdflatex -interaction=nonstopmode "..file..".tex")
-  end
-  -- Copying
-  os_message("** Copying "..file..".log and "..file..".pdf files to main dir: OK")
-  cp("example.log", tempdir, maindir)
-  cp("example.pdf", tempdir, maindir)
-  -- Clean
-  os_message("** Remove temporary directory ./"..tempdir..": OK")
-  cleandir(tempdir)
-  lfs.rmdir(tempdir)
-  os.exit()
-end
+# This job creates a formal release and attaches the artifacts from the build job.
+create_release:
+  stage: release
+  image: registry.gitlab.com/gitlab-org/release-cli:latest
+  # This job depends on the 'build_assets' job to be successful.
+  needs:
+    - job: build_assets
+      artifacts: true
+  script:
+    - echo "Creating release for tag $CI_COMMIT_TAG"
+    # Construct the versioned filename by stripping the 'v' from the Git tag.
+    - |
+      VERSION="${CI_COMMIT_TAG#v}"
+      VERSIONED_ZIP_FILENAME="maine-thesis-${VERSION}.zip"
+    # The release-cli command uploads the files directly from the artifacts.
+    # We use 'filepath' which is much simpler and more reliable than building a URL.
+    - release-cli create --name "Release $CI_COMMIT_TAG" --tag-name $CI_COMMIT_TAG \
+        --assets-link "{\"name\":\"maine-thesis.cls\", \"filepath\":\"build/unpacked/maine-thesis.cls\"}" \
+        --assets-link "{\"name\":\"maine-thesis.tds.zip\", \"filepath\":\"maine-thesis.tds.zip\"}" \
+        --assets-link "{\"name\":\"maine-thesis-template.zip\", \"filepath\":\"template.zip\"}" \
+        --assets-link "{\"name\":\"${VERSIONED_ZIP_FILENAME}\", \"filepath\":\"${VERSIONED_ZIP_FILENAME}\"}"
+  # This rule ensures the release job ONLY runs when a new Git tag is pushed.
+  rules:
+    - if: $CI_COMMIT_TAG
